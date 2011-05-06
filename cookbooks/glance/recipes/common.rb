1478774eebb336da89b2850ec2cf7b2c176c7f29
Dan Prince <dan.prince@rackspace.com>
2011-05-05 22:19:27 -0400

    Temporarily ignore failures on glance apt-get installs.

--- cookbooks/glance/recipes/common.rb
+++ cookbooks/glance/recipes/common.rb
@@ -7,6 +7,7 @@
 package "glance" do
   options "--force-yes"
   action :install
+  ignore_failure true
 end
 
 [node[:glance][:log_dir], node[:glance][:working_directory], File::dirname(node[:glance][:config_file]), node[:glance][:pid_directory]].each do |glance_dir|
