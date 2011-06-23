9d70aa6832b9cdd4e6ad82d656f988378154e5b1
Dan Prince <dan.prince@rackspace.com>
2011-06-23 14:15:05 -0400

    Update default image URL (for maverick).

--- cookbooks/xenserver/attributes/xenserver.rb
+++ cookbooks/xenserver/attributes/xenserver.rb
@@ -1,4 +1,4 @@
-default[:xenserver][:xva_image_url] = "http://c3324746.r46.cf0.rackcdn.com/maverick.xva"
+default[:xenserver][:xva_image_url] = "http://c3324746.r46.cf0.rackcdn.com/maverick_agent.xva"
 default[:xenserver][:image_path] = "/root/maverick_agent.xva"
 default[:xenserver][:license_filename] = "/root/.xenserver_license.txt"
 default[:xenserver][:sr_name_label] = "Local storage"
