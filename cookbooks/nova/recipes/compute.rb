fd120047eb5d891767b568a57c9ec5e3f090350a
Dan Prince <dan.prince@rackspace.com>
2011-06-23 09:05:00 -0400

    Update nova::compute recipe to support QEMU/KVM/XenServer.

--- cookbooks/nova/recipes/compute.rb
+++ cookbooks/nova/recipes/compute.rb
@@ -1,26 +1,30 @@
-#
-# Cookbook Name:: nova
-# Recipe:: compute
-#
-# Copyright 2010, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# NOTE: I manually reimplented the nova-compute startup here so that it
+# works on stock cloud servers. This works around the fact that stock
+# Ubuntu Cloud Servers images don't have the 'nbd' (network block device)
+# kernel module.
 
 include_recipe "nova::common"
-nova_package("compute")
 
-if node[:nova][:compute_connection_type] == "kvm"
+package "nova-compute" do
+  options "--force-yes"
+  action :install
+end
+
+template "/etc/init/nova-compute.conf" do
+  source "nova-compute.conf"
+  mode "0644"
+end
+
+if node[:nova][:connection_type] and node[:nova][:connection_type] == "xenapi" then
+
+  # FIXME: create a python XenAPI package
+  package "python-pip"
+  execute "/usr/bin/pip install xenapi" do
+    not_if "python -c 'import XenAPI'"
+  end
+
+elsif node[:nova][:connection_type] and node[:nova][:connection_type] == "kvm" then
+
   service "libvirt-bin" do
     notifies :restart, resources(:service => "nova-compute"), :immediately
   end
@@ -29,8 +33,44 @@ if node[:nova][:compute_connection_type] == "kvm"
     action :run
     notifies :restart, resources(:service => "libvirt-bin"), :immediately
   end
+
+  execute "modprobe nbd" do
+    action :run
+  end
+# default
+elsif not node[:nova][:connection_type] or node[:nova][:connection_type] == "libvirt" then
+
+  directory "/dev/cgroup" do
+    owner "root"
+    group "root"
+    mode "0755"
+    action :create
+  end
+
+  execute "mount -t cgroup none /dev/cgroup -o devices" do
+    not_if "mount | grep cgroup"
+  end
+
+  execute "apt-get -y --force-yes install libvirt0=#{node[:libvirt][:version]} libvirt-bin=#{node[:libvirt][:version]} python-libvirt=#{node[:libvirt][:version]}" do
+    not_if "dpkg -l libvirt-bin | grep #{node[:libvirt][:version]}"
+  end
+
+  service "libvirt-bin"
+
+  cookbook_file "/etc/libvirt/qemu.conf" do
+    source "qemu.conf"
+    mode "0644"
+    notifies :restart, resources(:service => "libvirt-bin")
+  end
+
 end
 
-execute "modprobe nbd" do
-  action :run
+service "nova-compute" do
+  restart_command "stop nova-compute; start nova-compute"
+  stop_command "stop nova-compute"
+  start_command "start nova-compute"
+  status_command "status nova-compute | cut -d' ' -f2 | cut -d'/' -f1 | grep start"
+  supports :status => true, :restart => true
+  action :start
+  subscribes :restart, resources(:template => ["/etc/nova/nova.conf", "/etc/init/nova-compute.conf"])
 end
