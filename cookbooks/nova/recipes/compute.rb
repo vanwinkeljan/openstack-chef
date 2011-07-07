63ba60bdfc2fedfd124501ffda70991afe575b81
Brian Lamar <brian.lamar@rackspace.com>
2011-07-07 14:38:52 -0400

    Just kidding, this is more simple than I thought.

--- cookbooks/nova/recipes/compute.rb
+++ cookbooks/nova/recipes/compute.rb
@@ -26,15 +26,6 @@ if node[:nova][:connection_type] and node[:nova][:connection_type] == "xenapi" t
     not_if "python -c 'import XenAPI'"
   end
 
-  package "uml-utilities" do
-    options "--force-yes"
-    action :install
-  end
-
-  execute "tunctl -t tap0" do
-    action:run
-  end
-
 elsif node[:nova][:connection_type] and node[:nova][:connection_type] == "kvm" then
 
   service "libvirt-bin" do
