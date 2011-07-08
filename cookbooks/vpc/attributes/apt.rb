b284c8908a10a99ab31aeac76100ed5b63413e44
William Wolf <throughnothing@gmail.com>
2011-07-08 11:36:29 -0400

    break out nginx into its own recipe.  update apt and yum recipes to use it properly.

--- cookbooks/vpc/attributes/apt.rb
+++ cookbooks/vpc/attributes/apt.rb
@@ -1,3 +1,3 @@
 default[:vpc][:apt][:distro] = "maverick"
 default[:vpc][:apt][:ppa_url] = "http://ppa.launchpad.net/nova-core/trunk/ubuntu"
-default[:vpc][:apt][:local_url] = "http://login.vpc/openstack"
+default[:vpc][:apt][:local_url] = "http://login.vpc/apt/openstack"
